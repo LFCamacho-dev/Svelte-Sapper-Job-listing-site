29d430cee1f053e345ba80b181c01cfc3774c2c1
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-06-21 01:41:16 -0500

    first commit

---
+++ src/routes/contact.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Contact Job Postings</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis recusandae omnis ipsa eveniet reiciendis neque repudiandae laudantium, officia nemo iusto consequuntur sint totam at repellat debitis aliquam modi laboriosam. Minus placeat asperiores suscipit aliquam mollitia quaerat dolorum sed. A vitae consequatur delectus ipsum, id vero ea blanditiis reprehenderit odio cumque!</p>
---
+++ src/routes/jobs/index.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<h3>All Current Jobs</h3>
